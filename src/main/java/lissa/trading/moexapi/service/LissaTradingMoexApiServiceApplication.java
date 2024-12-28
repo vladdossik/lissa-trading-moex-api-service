6c668e8ac2c2ac42726ae3047b4dceb7cab40d54
SashaTyutyaev <sashatut2002@yandex.ru>
2024-12-28 18:34:08 +0300

    feat(STOCK-59) added auth token filter

--- src/main/java/lissa/trading/moexapi/service/LissaTradingMoexApiServiceApplication.java
+++ src/main/java/lissa/trading/moexapi/service/LissaTradingMoexApiServiceApplication.java
@@ -2,8 +2,10 @@ package lissa.trading.moexapi.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "lissa.trading")
 public class LissaTradingMoexApiServiceApplication {
 
     public static void main(String[] args) {
