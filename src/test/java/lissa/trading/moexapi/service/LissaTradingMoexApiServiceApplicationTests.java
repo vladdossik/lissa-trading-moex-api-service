5b4f5b36cf91d0cd0004c5b0f76b54e09b2c4688
blaskowitz <blaskowitzwork@gmail.com>
2024-12-28 18:32:53 +0300

    patch-feat(STOCK-50) - fixed test

---
+++ src/test/java/lissa/trading/moexapi/service/LissaTradingMoexApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lissa.trading.moexapi.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LissaTradingMoexApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
